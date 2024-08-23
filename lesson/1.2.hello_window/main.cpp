c4dd8b7afea0ae2afb585dadf05c0db2cca59e17
kiridore <ashenone.jiang@gmail.com>
2024-08-23 18:18:42 +0800

    fix: add fremebuffer size callback

--- lesson/1.2.hello_window/main.cpp
+++ lesson/1.2.hello_window/main.cpp
@@ -1,15 +1,24 @@
 #include "common/app.hpp"
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 class ExampleHelloWindow : public Entry::AppI {
 public:
     ExampleHelloWindow(std::string name, std::string desc)
         : Entry::AppI(name, desc) {}
 
-    void Init() override { glViewport(0, 0, 800, 600); }
+    void Init() override {
+        glViewport(0, 0, 800, 600);
+        glfwSetFramebufferSizeCallback(GetMainWindow(), framebuffer_size_callback);
+    }
 
     bool Update() override {
         if (this->GetMainWindow() == nullptr) return false;
         if (glfwWindowShouldClose(GetMainWindow())) return false;
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         return true;
     }
